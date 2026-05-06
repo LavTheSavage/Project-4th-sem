c0f4a15c90a1e55a5514e64fad602c47d2c26c0b
LavTheSavage <your-main-email@example.com>
2026-05-06 12:43:43 +0545

    feat: add flutter_secure_storage and facebook_auth plugins for enhanced storage and authentication

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import facebook_auth_desktop
 import file_selector_macos
 import flutter_image_compress_macos
+import flutter_secure_storage_darwin
+import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -15,8 +18,11 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
